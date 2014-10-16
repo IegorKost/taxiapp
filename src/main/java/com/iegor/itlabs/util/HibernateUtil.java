bb995cd6d09cb3ad45c9565aadb3775ebf5735d8
Iegor <egor_kost@mail.ru>
2014-10-16 15:44:22 +0300

    update configuration of sessionFactory

--- src/main/java/com/iegor/itlabs/util/HibernateUtil.java
+++ src/main/java/com/iegor/itlabs/util/HibernateUtil.java
@@ -2,6 +2,7 @@ package com.iegor.itlabs.util;
 
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -11,7 +12,9 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory;
     static {
         try{
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration().configure();
+            StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(ssrb.build());
         }catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
